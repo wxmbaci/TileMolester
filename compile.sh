e4b6e086e862da4aa02f5f9d4c893881a1852178
wxm <v_wangjunming@baidu.com>
2023-08-18 16:04:46 +0800

    fix err

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 rm -rf ./build/*
 javac -sourcepath src -d ./build src/TileMolester.java
-cp -R resources/icons/ build/production/TileMolester/tm/
-cp resources/splash.gif build/production/TileMolester/tm/splash.gif
+cp -R resources/icons build/
+cp resources/splash.gif build/tm/splash.gif
 jar cvfm TileMolester.jar META-INF/MANIFEST.MF -C build/ .
